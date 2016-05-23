4f981397a054d1b8ab85ef778f269203695d0cfa
Raphael Serota <raphael.serota@gmail.com>
2016-05-23 19:35:30 +0000

    ...

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
 
 app.get('/', function(req, res){
-    res.send('Welcome to the internets!')
+    res.send('Welcome to the internet!')
 })
 
 app.listen(80)
