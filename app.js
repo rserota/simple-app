35ea70920c5b6c0cd0f0355a78a40ce063045da8
Raphael Serota <rserota@users.noreply.github.com>
2016-05-23 17:06:40 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
 var express = require('express')
 var app = express()
+var http = require('http')
+var https = require('https')
 
 app.get('/', function(req, res){
     console.log('someone visited the home page!')
     res.send('Welcome to the internet!')
 })
+var httpServer = http.createServer(app)
+httpServer.listen(80)
 
-app.listen(80)
+// HTTPS Setup //
+try {
+
+    var credentials = {
+      key: fs.readFileSync('/etc/letsencrypt/live/thepasswordisdragons.com/privkey.pem'),
+      cert: fs.readFileSync('/etc/letsencrypt/live/thepasswordisdragons.com/cert.pem')
+    };
+    
+    https.createServer(credentials, app).listen(443);
+}
+catch(error){
+    console.log('HTTPS setup failed.')
+    console.log('=-=-=-=-=-=-=-=-=-=-=')
+    console.log(error)
+
+}
+
